702d8ec368995c8bb3ee6d2ec936ad75ccd6bb23
MishaGaer <gaer1997@yandex.ru>
2022-06-24 09:53:01 +0200

    third_home_work

---
+++ Home_work_3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, 
+которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.WriteLine("Please, enter the number:");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Number {number} is EVEN");
+}
+else
+{
+    Console.WriteLine($"Number {number} is NOT EVEN");
+}
